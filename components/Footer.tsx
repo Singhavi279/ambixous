269ba73f1f6ea831ae6908181c3c6d276ee8ab52
AVNISH SINGH <38598925+Singhavi279@users.noreply.github.com>
2025-06-14 01:04:20 +0530

    modify

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-midnight text-sm text-center py-6 border-t border-lilac">
-      <p className="text-white">© {new Date().getFullYear()} Ambixous. Built to Inspire. Coded to Spark.</p>
-    </footer>
-  );
-}
